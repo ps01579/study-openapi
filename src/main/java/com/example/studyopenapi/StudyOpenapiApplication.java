458fe571ea48349f8fed03f14d4ad2324a73abf3
mgilpark <mgilpark@naver.com>
2024-04-11 00:28:00 +0900

    Initial commit

---
+++ src/main/java/com/example/studyopenapi/StudyOpenapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyOpenapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyOpenapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyopenapi/StudyOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyOpenapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
